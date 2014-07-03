a2b21b1dffbf4bb5ffee60aa7baf019dc8f24255
Erik Landerholm <elanderholm@truecar.com>
2014-07-03 13:20:58 -0700

    fart

--- lib/sharting.rb
+++ lib/sharting.rb
@@ -1,5 +1,5 @@
 module Sharting
-  SHARD_COUNT = 4
+
 
   def self.enabled?
     Octopus.enabled?
@@ -10,8 +10,11 @@ module Sharting
   end
 
   def self.shard_for_key(key)
-    shard_number = Digest::SHA2.hexidigest(key).to_i(16) % SHARD_COUNT
-    :"octopussy_shard_#{shard_number}"
+    :"shard_#{shard_number(key)}"
+  end
+
+  def self.shard_number(key)
+    shard_number = Digest::SHA1.hexdigest(key).to_i(16) % number_of_shards
   end
 
   def self.using(shard, &block)
@@ -30,5 +33,20 @@ module Sharting
   def self.database_name(shard_name)
     connection_proxy = ActiveRecord::Base.connection
     connection_proxy.send(:database_name, shard_name)
+  end
+
+  def self.number_of_shards
+    Rails.configuration.number_of_shards
+  end
+
+  def self.generate_uid(key)
+    key(key) do
+      sql = "select shard_nextval() as next_seq, now_msec() as msec"
+      next_seq, msec = ActiveRecord::Base.connection.execute(sql).first
+
+      uid = msec << (64-41)
+      uid |= shard_number(key) << (64-41-13)
+      uid |= next_seq
+    end
   end
 end
