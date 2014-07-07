b09d624e5b422c1545796e6a24af7acdd87fc917
Scott Holden <sholden@truecar.com>
2014-07-07 16:17:21 -0700

    removing float CSV converter to try to get around data loader segfault

--- lib/data_loader.rb
+++ lib/data_loader.rb
@@ -22,7 +22,7 @@ class DataLoader
     prices_by_vehicle = CSV.open(data_path + '/price.csv', headers: price_headers, converters: [:all, bool_converter]).group_by{|r| r[:vehicle_id]}
 
     option_headers = [:vehicle_id, :description, :opt_code, :is_quick_package, :is_option_package, :is_dio_option, :msrp, :invoice, :opt_kind]
-    options_by_vehicle = CSV.open(data_path + '/option.csv', headers: option_headers, converters: [:all, bool_converter]).group_by{|r| r[:vehicle_id]}
+    options_by_vehicle = CSV.open(data_path + '/option.csv', headers: option_headers, converters: [:integer, bool_converter]).group_by{|r| r[:vehicle_id]}
 
     buyers_by_id.each do |buyer_id, buyer_attributes|
       buyer_attributes = buyer_attributes.to_hash
@@ -35,13 +35,13 @@ class DataLoader
           vehicle = user.vehicles.create!(vehicle_attributes.except(:user_id))
 
           Array(prices_by_vehicle[vehicle_id]).each do |price_attributes|
-            price_attributes = price_attributes.to_hash
-            vehicle.prices.create!(price_attributes.except(:vehicle_id))
+            price_attributes = price_attributes.to_hash.except(:vehicle_id)
+            vehicle.prices.create!(price_attributes)
           end
 
           Array(options_by_vehicle[vehicle_id]).each do |option_attributes|
-            option_attributes = option_attributes.to_hash
-            vehicle.options.create!(option_attributes.except(:vehicle_id))
+            option_attributes = option_attributes.to_hash.except(:vehicle_id)
+            vehicle.options.create!(option_attributes)
           end
         end
 
